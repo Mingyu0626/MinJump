f25402c76906593f97a627fefb7db0caeeda87bb
Mingyu Choi <mg010626@naver.com>
2024-03-07 17:30:21 +0900

    Add | 함수 설명 주석 추가

--- Source/PuzzlePlatforms/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -13,6 +13,8 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// HasAutority() 가 true라는 뜻은, 우리가 서버에서 실행중임을 의미한다. 
+	// false이면 클라이언트에서 실행중임을 의미한다.
 	if (HasAuthority())
 	{
 		FVector Location = GetActorLocation();
